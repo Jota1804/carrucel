1c6984ff425501a2cad0fe9482f44a25ed9c03d6
Jota1804 <100544185+Jota1804@users.noreply.github.com>
2024-03-08 16:38:37 -0400

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,38 +1,38 @@
 const swiper = new Swiper('.swiper', {
 
 
-    autoplay: {
-        delay: 5000,
-        pauseOnMouseEnter: true,
-        disableOnInteraction: false,
-    },
+  autoplay: {
+    delay: 5000,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+  },
 
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
   
 
-/*    
+
     // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true,
-    },
-*/
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
 
     // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-/*
+
     // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-      draggable: true,
-    },
-*/
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+
 
   });
